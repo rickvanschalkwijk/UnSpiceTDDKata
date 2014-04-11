c9888d2fd8cda1d9625fcf5a3ef5c4bddb62c43f
Rick van Schalkwijk <rvanschalkwijk@mirabeau.nl>
2014-04-11 11:36:36 +0200

    added second tests and method to remove multiple slashes in the string

--- UnspliceTDDKata/UnspliceTDDKataTests.cs
+++ UnspliceTDDKata/UnspliceTDDKataTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
 
 namespace UnspliceTDDKata
 {
@@ -6,13 +9,26 @@ namespace UnspliceTDDKata
     public class UnspliceTDDKataTests
     {
         [Test]
-        public void RemoveBackSlashes_ShouldRemoveBackSlashesFromString_ReturnStringWithoutBackSlashes()
+        public void RemoveBackSlashes_ShouldRemoveSingleBackSlashesFromString_ReturnStringWithoutBackSlashes()
         {
             // Arrange
             Unsplice unsplice = new Unsplice();
 
             // Act
-            var actual = unsplice.RemoveBackSlashes(@"abc\\\def");
+            var actual = unsplice.RemoveBackSlashes(@"abc\\\ndef");
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("abcdef"));
+        }
+
+        [Test]
+        public void RemoveBackSlashes_ShouldRemoveDoubleBackSlashesFromString_ReturnStringWithoutBackSlashes()
+        {
+            // Arrange
+            Unsplice unsplice = new Unsplice();
+
+            // Act
+            var actual = unsplice.RemoveBackSlashes(@"abc\\\nde\\\nf");
 
             // Assert
             Assert.That(actual, Is.EqualTo("abcdef"));
@@ -23,7 +39,8 @@ namespace UnspliceTDDKata
     {
         public string RemoveBackSlashes(string s)
         {
-            return "abcdef";
+            var splittedArray = s.Split(new string[] { "\\\\\\n" }, StringSplitOptions.None);
+            return string.Join("",splittedArray);
         }
     }
 }
