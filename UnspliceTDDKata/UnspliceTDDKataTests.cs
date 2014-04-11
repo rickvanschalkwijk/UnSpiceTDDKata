74c2515a0eb870bc6b5e3e92af18d2d7365a3e83
Rick van Schalkwijk <rvanschalkwijk@mirabeau.nl>
2014-04-11 11:17:30 +0200

    written first test to implement class and method

--- UnspliceTDDKata/UnspliceTDDKataTests.cs
+++ UnspliceTDDKata/UnspliceTDDKataTests.cs
@@ -1,7 +1,29 @@
-﻿namespace UnspliceTDDKata
+﻿using NUnit.Framework;
+
+namespace UnspliceTDDKata
 {
+    [TestFixture]
     public class UnspliceTDDKataTests
     {
-         
+        [Test]
+        public void RemoveBackSlashes_ShouldRemoveBackSlashesFromString_ReturnStringWithoutBackSlashes()
+        {
+            // Arrange
+            Unsplice unsplice = new Unsplice();
+
+            // Act
+            var actual = unsplice.RemoveBackSlashes(@"abc\\\def");
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("abcdef"));
+        }
+    }
+
+    public class Unsplice
+    {
+        public string RemoveBackSlashes(string s)
+        {
+            return "abcdef";
+        }
     }
 }
